dd845c6bad212cd1772190c1a7132fd24708fa6e
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 00:22:59 +0545

    learned about variables

---
+++ comments.js
@@ -0,0 +1,13 @@
+// there is two way to write comment
+// 1.single line comments
+// this is single line comment
+
+// Multi line comments
+/*
+API Documentation or logic expression
+My 
+name 
+is 
+Anjali
+*/
+// cntrl+/ (for single comment)
---
+++ variable.js
@@ -0,0 +1,26 @@
+//  ES5
+var person = "anjali jaiswal"; //(Global variables)
+
+//ES6
+const anotherPerson = "aachal";
+let thirdPerson = "juhi";
+console.log(person, anotherPerson, thirdPerson);
+
+var test = "anjali";
+var test = "shree ram";
+console.log(test);
+
+const testPerson = "raktim"; //(fixed variable name declare)
+console.log(testPerson);
+
+let newPerson = "anjali"; //(variable update garna mildo raixa)
+newPerson = "ramayan";
+console.log(newPerson);
+
+//Always start variable with const keywords
+const table = "red table";
+console.log(table); // red table
+
+let phone = "9855040483";
+phone = "9823148049";
+console.log(phone); //9823148049
