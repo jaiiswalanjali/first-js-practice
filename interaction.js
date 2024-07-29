b34114b1b4e334104ea714df41c2549db8e4572f
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 09:32:27 +0545

    learned about interactions

---
+++ interaction.js
@@ -0,0 +1,16 @@
+//User interface
+
+//alert
+// alert('Hello world');
+
+//prompt
+const age=prompt('how old are you?');
+
+//confirm
+const above18 =confirm('Are you above 18 years old ?')
+
+console.log(age,above18)
+
+alert(`you are $(age) year old`);//string literal
+
+//write a js program to create sum calculator using user interaction
