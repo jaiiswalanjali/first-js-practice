be79aa72122fd4423fe0531c1d4eece38fb8115d
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 11:57:28 +0545

    updated

--- interaction.js
+++ interaction.js
@@ -15,6 +15,7 @@
 
 // //write a js program to create sum calculator using user interaction
 const num1=prompt("Enter first number");
+console.log(typeof num1);
 const num2=prompt("Enter second number");
 const sum=Number(num1)+Number(num2);
 alert(`The sum of number ${num1} and ${num2} is ${sum}`);
