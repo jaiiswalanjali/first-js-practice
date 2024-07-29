1c17b869d556e3bf8e3888715aa1bbbdd798cdeb
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 11:55:51 +0545

    completing some  calculator assignment

---
+++ data-type.js
@@ -0,0 +1,28 @@
+//data types
+/* 1.String
+2.Number
+3.Bigint
+4.Boolean
+5.Undefined
+6.Null
+7.Symbol
+
+
+8.Object*/
+// the object data type 
+/* 1.an object
+2.an array
+3.a date*/
+
+//string
+const x="2";
+console.log(x);
+
+//to convert string into number 
+const y=Number(x);
+console.log(y);
+
+ // covert Number to string
+ const z=String(y);
+ console.log(z);
+ 
--- interaction.js
+++ interaction.js
@@ -1,16 +1,21 @@
-//User interface
+// //User interface
 
-//alert
-// alert('Hello world');
+// //alert
+// // alert('Hello world');
 
-//prompt
-const age=prompt('how old are you?');
+// //prompt
+// const age=prompt('how old are you?');
 
-//confirm
-const above18 =confirm('Are you above 18 years old ?')
+// //confirm
+// const above18 =confirm('Are you above 18 years old ?')
 
-console.log(age,above18)
+// console.log(age,above18)
 
-alert(`you are $(age) year old`);//string literal
+// alert(`you are $(age) year old`);//string literal
 
-//write a js program to create sum calculator using user interaction
+// //write a js program to create sum calculator using user interaction
+const num1=prompt("Enter first number");
+const num2=prompt("Enter second number");
+const sum=Number(num1)+Number(num2);
+alert(`The sum of number ${num1} and ${num2} is ${sum}`);
+console.log(sum)
