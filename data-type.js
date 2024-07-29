55f4ea7993177ce26c875fc6c21d182624a1a006
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 12:03:27 +0545

    updated

--- data-type.js
+++ data-type.js
@@ -1,5 +1,7 @@
 //data types
-/* 1.String
+//two type primitivr data type and non primitive data type
+/* primitive or simple data type
+1.String
 2.Number
 3.Bigint
 4.Boolean
@@ -8,12 +10,13 @@
 7.Symbol
 
 
-8.Object*/
+8.Object* (complex data type or non primitive data type)/
 // the object data type 
 /* 1.an object
 2.an array
 3.a date*/
 
+//type of conversion or coercion
 //string
 const x="2";
 console.log(x);
@@ -25,4 +28,6 @@ console.log(y);
  // covert Number to string
  const z=String(y);
  console.log(z);
- 
+
+ // TO KNOW Type of data
+ // console.log(typeof x)
