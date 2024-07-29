5986c4f2dc38dc9d8608c8d971ee4672e59332f5
Anjali jaiswal <91237205+jaiiswalanjali@users.noreply.github.com>
2024-07-29 16:41:41 +0545

    learned about statements

---
+++ statement.js
@@ -0,0 +1,89 @@
+//statement
+// 1.if else
+//2.switch case
+
+/* if else
+syntax
+if(condition){
+// if logic(if true)
+} else{
+ //else logic(if false) } */
+
+/*  const gender=confirm("are you a male");
+//  if (gender){
+//     alert("you are male");// if true
+//  }
+//  else{
+//     alert("you are female")//if false
+ } */
+  
+ // ask user to reply m or f type
+
+  /*const gender=prompt("enter m for male or f for female");
+ if (gender==="m"){
+    alert("you are male");// if true
+ }
+ else{
+    alert("you are female")//if false
+ } */
+
+ // =( to assign variable)
+ // ==( use to know the data type and value comparision)
+ // ===( use to know the data type and value comparision and to convert data to boolean)
+
+ // switch case
+ /*
+ case "1":
+    alert ('sunday');
+    break;
+    case "2":
+        alert('Monday');
+        break;
+        default:
+            alert("You have to choose between 1 to 7")
+ */
+
+            // write a js program to check the day of the week 
+        //     const day = Number(prompt("Enter number 1, 2, or 3"));
+        //    switch (day) {
+        //     case "1":
+        //         alert("sunday");
+        //         break;
+        //         case "2":
+        //             alert("Monday");
+        //             break;
+        //             case "3":
+        //             alert("Tuesday");
+        //             break;
+        //             default:
+        //                 alert("you must choose between 1 to 3")
+        //    }
+      
+        //    // write a js program to check the grade of the student
+        //    // a:>=80
+        //    //b:>=60but less than 80
+        //    const score=Number(prompt("Enter your score"));
+        //    switch (true){
+        //     case score >= 80:
+        //     alert("you grade is A");
+        //         break;
+        //         case score >=60 && score<80:
+        //             alert("you grade is B");
+        //             break;
+        //             default:
+        //                 alert("you have failed.better luck next time")
+        //    }
+        // if{}else{} ES5
+        //conditional operator(ternary operator) //ES6
+        const gender="m";
+        //if (gender ==="m"){
+       // console.log("m"); 
+       // } else{
+       
+       //ES6
+       //syntax
+       //condition? true? false;
+       gender==="m"?console.log("m"):console.log("f"); //m
+
+
+ 
